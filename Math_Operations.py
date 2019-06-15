67048fa5f61a6072e81d1e60b2511a722a1f20e4
Sathish Manthani <smanthani@my365.bellevue.edu>
2019-06-14 23:45:40 -0700

    New file

--- Math_Operations.py
+++ Math_Operations.py
@@ -1,2 +1,83 @@
- m
- <''
+# Addition
+a = 100 + 60
+print (a)  # Prints value 160
+# Subraction
+a = 100 - 60
+print (a)  # Prints value 40
+# Multiplication
+a = 20 * 30
+print (a)  # Prints value 600
+# Division
+a = 80 / 20
+print (a)  # Prints value 4
+# remainder
+a = 51 % 2
+print (a)  # Prints value 0
+# power
+a = 20 ** 3
+print (a)  # Prints value 80000
+
+print (4 / 2)
+
+print (4 // 2)
+
+a = 1
+b = 2
+c = a / b
+print (a, "/", b, "=", c)
+
+add = str (a) + "/" + str (b) + "=" + str (c)
+print (add)
+
+a = "How"
+b = "are"
+c = "you"
+print (a, b, c)  # Prints "How are  you"
+print (a + b + c)  # Prints "Howareyou"
+
+var1 = 10
+print (type (var1))
+
+var1 = "abc"
+print (type (var1))
+
+str1 = "Hello "
+var1 = 3
+print (var1 * str1)
+
+str1 = "Pie is "
+var2 = 3.14
+str3 = str1 + str (var2)
+print (str3)
+
+a = 3 + 2j
+print (a)
+print (type (a))
+print (a.real)
+print (a.imag)
+
+a = abs (-2.22)
+
+print (a)
+
+a = -1.8
+b = 2.9
+print (abs (a))
+print (abs (b))
+
+var1 = 1
+# print("its integer:",var1.is_integer())
+# a=1.0.is_integer()
+# print(a)
+# a=121.3123
+
+a = 1.1
+b = 10.0
+print (a.is_integer ())
+# Outputs False
+print (b.is_integer ())
+# Outputs True
+
+str1 = "abc"
+print (str1.isdigit ())
+
